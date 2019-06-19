9ebe4ba969847cd26fe3da50f29b238a4f279e8b
Andrew H <ahennesen@gmail.com>
2019-06-19 18:27:56 -0400

    added url true parse

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ var db = require("./models");
 
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 
-mongoose.connect(MONGODB_URI);
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
 
 var PORT = 3000;
 
